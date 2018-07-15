482779cc6c58475d3434c02d548ab75d53324e96
yunus emre rendecioğlu <yunus-sakarya54@hotmail.com>
2018-07-15 14:21:14 +0300

    Araçtkaipharitada yeni sqlclas

---
+++ USQLCSharpProject1/USQLCSharpProject1/Class1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Interfaces.Streaming;
+using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace USQLCSharpProject1
+{
+    public class Class1
+    {
+        //araçTakip dalında
+    }
+}
